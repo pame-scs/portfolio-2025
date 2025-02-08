5d85301db336a6ace26deab0c0e743657e06e80e
Pamela Castaneda <pamela.castaneda-sandoval@etu.unistra.fr>
2025-02-08 19:04:43 +0100

    Enhance navigation language links with active state styling and update CSS for improved appearance

--- php/nav.php
+++ php/nav.php
@@ -1,8 +1,8 @@
 <nav>
 <div class="traduction">
-    <a href="?<?php echo isset($_GET['id']) ? 'id=' . htmlspecialchars($_GET['id']) . '&' : ''; ?>lang=fr">Français</a>
-    <a href="?<?php echo isset($_GET['id']) ? 'id=' . htmlspecialchars($_GET['id']) . '&' : ''; ?>lang=en">English</a>
-    <a href="?<?php echo isset($_GET['id']) ? 'id=' . htmlspecialchars($_GET['id']) . '&' : ''; ?>lang=esp">Español</a>
+    <a href="?<?php echo isset($_GET['id']) ? 'id=' . htmlspecialchars($_GET['id']) . '&' : ''; ?>lang=fr" class="<?php echo (isset($_GET['lang']) && $_GET['lang'] == 'fr') ? 'active' : ''; ?>">Français</a>
+    <a href="?<?php echo isset($_GET['id']) ? 'id=' . htmlspecialchars($_GET['id']) . '&' : ''; ?>lang=en" class="<?php echo (isset($_GET['lang']) && $_GET['lang'] == 'en') ? 'active' : ''; ?>">English</a>
+    <a href="?<?php echo isset($_GET['id']) ? 'id=' . htmlspecialchars($_GET['id']) . '&' : ''; ?>lang=esp" class="<?php echo (isset($_GET['lang']) && $_GET['lang'] == 'esp') ? 'active' : ''; ?>">Español</a>
 </div>
     <a href="#home"><?php echo $TRAD["home"] ?></a>
     <a href="#about"><?php echo $TRAD["skills"] ?></a>
