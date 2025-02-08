39340bf0db719743bd9e828a1dd7f70831e0158c
Pamela Castaneda <pamela.castaneda-sandoval@etu.unistra.fr>
2025-02-08 18:38:16 +0100

    Add Google Fonts and update styles for landing page

--- traduction/esp.php
+++ traduction/esp.php
@@ -8,7 +8,7 @@ $translations = [
     /* --------------------------------- Landing -------------------------------- */
     "job" => "Estudiante",
     "about" => "Hola! Soy Pamela y a través de mis habilidades en diseño, desarrollo web y gestión de proyectos, quiero crear soluciones atractivas e impactantes en la era digital. Siempre estoy lista para el próximo desafío: ¡explora mi viaje y mis logros!",
-    "project-button" => "Mira mis proyectos",
+    "project-button" => "Descubre mis proyectos",
     "contact-button" => "Contáctame"
 ];
 ?>
