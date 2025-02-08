6ea670098bfeae9c9c70dc155d2e8f7bee33454d
Pamela Castaneda <pamela.castaneda-sandoval@etu.unistra.fr>
2025-02-08 19:20:57 +0100

    Update 'about' section in translations for improved clarity and consistency

--- traduction/eng.php
+++ traduction/eng.php
@@ -7,7 +7,7 @@ $translations = [
     "projects" => "Projects",
     /* --------------------------------- Landing -------------------------------- */
     "job" => "Student | Web Developer | Community manager",
-    "about" => "Hello! I’m Pamela and through my skills in design, web development and project management, I want to create attractive and impactful experiences in the digital age. I am always ready for the next challenge: explore my journey and achievements!",
+    "about" => "Hello! I’m Pamela and through my skills in design, web development and project management, I want to create attractive and impactful experiences in the digital world. I am always ready for the next challenge: explore my journey and achievements!",
     "project-button" => "Check out my projects",
     "contact-button" => "Contact me"
 ];
--- traduction/esp.php
+++ traduction/esp.php
@@ -7,7 +7,7 @@ $translations = [
     "projects" => "Proyectos",
     /* --------------------------------- Landing -------------------------------- */
     "job" => "Estudiante | Desarrolladora web | Community manager",
-    "about" => "Hola! Soy Pamela y a través de mis habilidades en diseño, desarrollo web y gestión de proyectos, creo experiencias atractivas e impactantes en la era digital. Siempre estoy lista para el próximo desafío: ¡explora mi viaje y mis logros!",
+    "about" => "Hola! Soy Pamela y a través de mis habilidades en diseño, desarrollo web y gestión de proyectos, creo experiencias atractivas e impactantes en el mundo digital. Siempre estoy lista para el próximo desafío: ¡explora mi viaje y mis logros!",
     "project-button" => "Descubre mis proyectos",
     "contact-button" => "Contáctame"
 ];
