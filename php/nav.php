3d2f74bab04a1256ade4226cca8e1298f250c573
Pamela Castaneda <pamela.castaneda-sandoval@etu.unistra.fr>
2025-02-23 14:44:27 +0100

    Refactor navigation structure and styles for improved layout and responsiveness

--- php/nav.php
+++ php/nav.php
@@ -1,11 +1,19 @@
 <nav>
-<div class="traduction">
-    <a href="?<?php echo isset($_GET['id']) ? 'id=' . htmlspecialchars($_GET['id']) . '&' : ''; ?>lang=fr" class="<?php echo (isset($_GET['lang']) && $_GET['lang'] == 'fr') ? 'active' : ''; ?>">Français</a>
-    <a href="?<?php echo isset($_GET['id']) ? 'id=' . htmlspecialchars($_GET['id']) . '&' : ''; ?>lang=en" class="<?php echo (isset($_GET['lang']) && $_GET['lang'] == 'en') ? 'active' : ''; ?>">English</a>
-    <a href="?<?php echo isset($_GET['id']) ? 'id=' . htmlspecialchars($_GET['id']) . '&' : ''; ?>lang=esp" class="<?php echo (isset($_GET['lang']) && $_GET['lang'] == 'esp') ? 'active' : ''; ?>">Español</a>
-</div>
-    <a href="#home"><?php echo $TRAD["home"] ?></a>
-    <a href="#about"><?php echo $TRAD["skills"] ?></a>
-    <a href="#timeline"><?php echo $TRAD["timeline"] ?></a>
-    <a href="#"><?php echo $TRAD["projects"] ?></a>
+    <ul class="nav-a">
+        <li><a href="#home"><?php echo $TRAD["home"] ?></a></li>
+        <li><a href="#about"><?php echo $TRAD["skills"] ?></a></li>
+        <li><a href="#timeline"><?php echo $TRAD["timeline"] ?></a></li>
+        <li><a href="#"><?php echo $TRAD["projects"] ?></a></li>
+    </ul>
+
+    <img src="/assets/pame.png" alt="logo: pame scs">
+
+    <ul class="traduction">
+        <li><a href="?<?php echo isset($_GET['id']) ? 'id=' . htmlspecialchars($_GET['id']) . '&' : ''; ?>lang=fr"
+            class="<?php echo (isset($_GET['lang']) && $_GET['lang'] == 'fr') ? 'active' : ''; ?>">Français</a></li>
+        <li><a href="?<?php echo isset($_GET['id']) ? 'id=' . htmlspecialchars($_GET['id']) . '&' : ''; ?>lang=en"
+            class="<?php echo (isset($_GET['lang']) && $_GET['lang'] == 'en') ? 'active' : ''; ?>">English</a></li>
+        <li><a href="?<?php echo isset($_GET['id']) ? 'id=' . htmlspecialchars($_GET['id']) . '&' : ''; ?>lang=esp"
+            class="<?php echo (isset($_GET['lang']) && $_GET['lang'] == 'esp') ? 'active' : ''; ?>">Español</a></li>
+    </ul>
 </nav>
